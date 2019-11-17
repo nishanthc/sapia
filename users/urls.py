f0dacc57433e642ed0e73d98da62649f54490133
Nish chandradas <nishanthchandradas@gmail.com>
2019-11-17 01:53:57 +0000

    Added create/update merchant account page

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from users.views import ProfileView, MerchantCreateView
+from users.views import ProfileView, MerchantCreateView, MerchantUpdateView
 
 app = "users"
 urlpatterns = [
     path('accounts/profile/', ProfileView.as_view(), name='account_profile'),
-    path('accounts/merchant/create/', MerchantCreateView.as_view(), name="account_create_merchant")
+    path('accounts/merchant/create/', MerchantCreateView.as_view(), name="account_create_merchant"),
+    path('accounts/merchant/update/', MerchantUpdateView.as_view(), name="account_update_merchant")
+
 ]
--- users/views.py
+++ users/views.py
@@ -30,12 +30,24 @@ class MerchantCreateView(CreateView):
     template_name = 'account/merchant_create.html'
     success_url = reverse_lazy('account_create_merchant')
 
-
     def form_valid(self, form):
         if Merchant.objects.filter(account_id=self.request.user.id).exists():
             messages.add_message(self.request, messages.ERROR, 'You already have a merchant account!')
             return super(MerchantCreateView, self).form_invalid(form)
         form.instance.account = self.request.user
         messages.add_message(self.request, messages.SUCCESS, 'Merchant successfully created')
-
         return super(MerchantCreateView, self).form_valid(form)
+
+
+class MerchantUpdateView(UpdateView):
+    model = Merchant
+    form_class = MerchantCreationForm
+    template_name = 'account/merchant_update.html'
+    success_url = reverse_lazy('account_update_merchant')
+
+    def get_object(self, queryset=None):
+        return self.request.user.merchant
+
+    def form_valid(self, form):
+        messages.add_message(self.request, messages.INFO, 'Merchant account successfully updated')
+        return super(MerchantUpdateView, self).form_valid(form)
