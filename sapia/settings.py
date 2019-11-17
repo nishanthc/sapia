96394a958b0dff86c14e900dad62411fcc4ae4f6
Nish chandradas <nishanthchandradas@gmail.com>
2019-11-17 14:50:26 +0000

    updated settings

--- sapia/settings.py
+++ sapia/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
-# STATICFILES_DIRS = ( os.path.join('static'), )
+STATIC_ROOT = '/static'
+STATICFILES_DIRS = ( os.path.join('templates/static'), )
 
 AUTH_USER_MODEL = 'users.Account'
 
