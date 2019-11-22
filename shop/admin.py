27ab83447d45b83223f325f7c3443364f49323a8
Nish chandradas <nishanthchandradas@gmail.com>
2019-11-22 23:24:25 +0000

    Added Product model

--- shop/admin.py
+++ shop/admin.py
@@ -2,14 +2,23 @@ from adminsortable2.admin import SortableAdminMixin
 from django.contrib import admin
 
 # Register your models here.
-from shop.models import Category
+from shop.models import Category, Product
 
 
 class CategoryAdmin(SortableAdminMixin, admin.ModelAdmin):
     model = Category
     search_fields = ['name']
-    list_display = ['name','primary']
+    list_display = ['name', 'primary']
     exclude = ('slug',)
 
 
 admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    search_fields = ['name', 'merchant']
+    list_display = ['name', 'merchant']
+
+
+admin.site.register(Product, ProductAdmin)
--- shop/models.py
+++ shop/models.py
@@ -1,9 +1,65 @@
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
 # Create your models here.
 from django.urls import reverse
 from django.utils.text import slugify
 from django_extensions.db.models import TimeStampedModel
+
+
+class Product(TimeStampedModel):
+    merchant = models.ForeignKey(
+        'users.Merchant',
+        on_delete=models.CASCADE,
+        related_name="products",
+
+    )
+
+    name = models.CharField(
+        "name",
+        max_length=1024
+    )
+
+    description = models.TextField(
+        "Description",
+        max_length=1024,
+        blank=True,
+    )
+
+    price = models.IntegerField(
+        "price"
+    )
+
+    attributes = JSONField(
+        blank=True,
+        null=True
+    )
+
+    category = models.ManyToManyField(
+        "category",
+        related_name="products",
+    )
+
+    slug = models.SlugField(
+        unique=True,
+        blank=True
+    )
+
+    stock = models.IntegerField(
+        "stock",
+        null=True,
+        blank=True
+    )
+
+    def save(self, *args, **kwargs):
+        self.slug = self.slug or slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name.title()
+
+    def get_absolute_url(self):
+        return reverse('product-detail', kwargs={'slug': self.slug})
 
 
 class Category(TimeStampedModel):
